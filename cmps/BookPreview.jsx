df71bbd1d79ae5f151da88db8dc468fbd773ff8e
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-03-11 13:52:35 +0200

    Add more styling

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -2,7 +2,13 @@
 export function BookPreview({ book }) {
 	return <article className="book-preview">
 		<img src={book.thumbnail} />
-		<h2>{book.title}</h2>
-		<h4>{book.authors}</h4>
+		<div className='title-and-author'>
+			<h2>{book.title}</h2>
+			<h4>{book.authors}</h4>
+		</div>
+		<div className='price-and-rating hidden'>
+			<h2>{book.listPrice.amount} {book.listPrice.currencyCode}</h2>
+			<h4>{'****'}</h4>
+		</div>
 	</article>
 }
